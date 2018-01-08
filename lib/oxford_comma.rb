29f0eafe3a087a9d20a5fc92098b76c45f03854c
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-08 22:36:46 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.length == 3 
     return "#{array[0]}, #{array[1]}, and #{array[2]}"
-  elsif array.length > 3
+  else array.length > 3
     "#{array.last}"_cat = array.pop
     array.join(",") #<< " and #{"#{array.last}"}"
   end
