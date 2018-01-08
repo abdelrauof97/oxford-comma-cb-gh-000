55230a5a87ae6320fd5c1f50091bd441ffa09285
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-08 22:24:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,8 +7,7 @@ def oxford_comma(array)
   elsif array.length == 3 
     return "#{array[0]}, #{array[1]}, and #{array[2]}"
   elsif array.length > 3
-    array.pop("")
-    array.join(",") << " and #{array.last}"
+    array.pop.join(",") << " and #{array.last}"
   end
 end
 puts oxford_comma([1,2,3,4])
