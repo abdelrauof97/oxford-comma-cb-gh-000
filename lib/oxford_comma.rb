62a408e461f0d42de5666694ce784cd4aee48c1c
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-08 22:15:41 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
   elsif array.length == 3 
     return "#{array[0]}, #{array[1]}, and #{array[2]}"
   elsif array.length > 3
-    array.join(",") << "and"
+    array.join(",") << array.last.join."and"
   end
 end
 puts oxford_comma([1,2,3,4])
